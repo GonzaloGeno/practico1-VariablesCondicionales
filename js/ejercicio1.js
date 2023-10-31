e412dead134a54d2f8cf189df9e9453afb3744e8
Gonzalo Genini <gonzalogenini@gmail.com>
2023-10-30 22:39:23 -0300

    Se hizo el primer ejercicio

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -2,3 +2,23 @@
 
 //Crea dos variables, numero1 y numero2, y asígnales valores numéricos. Luego, realiza las siguientes operaciones y muestra los resultados en la consola:
 
+// Suma
+
+let numero1 = 10;
+let numero2 = 5;
+let resultado = null
+
+resultado = numero1 + numero2;
+console.log("El resultado de la suma es: "+ resultado);
+
+// Resta
+resultado = numero1 - numero2;
+console.log("El resultado de la resta es: "+ resultado);
+
+// Multiplicacion
+resultado = numero1 * numero2;
+console.log("El resultado de la multiplicacion es: "+ resultado);
+
+// Division
+resultado = numero1 / numero2;
+console.log("El resultado de la division es: "+ resultado)
